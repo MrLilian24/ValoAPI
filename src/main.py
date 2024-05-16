7846cd22891b30c2b14a9be4e7300ea5472a1440
Rémi LAVERGNE <remi.lavergne@etu.uca.fr>
2024-05-17 01:29:20 +0200

    Split into functions for the name and puuid

--- src/main.py
+++ src/main.py
@@ -14,16 +14,14 @@ def get_player_puuid(name, tag):
     except ApiError as e:
         print("Player cannot be found.\n", e)
 
-try:
-    player_puuid = "B0s7DeR8XNKCcDdFW6TpnRdY7cNBiFXpAWbhZaNk53sOkUQboogZxjwHdoCbWeOrnHxn9hH5T-aJlQ"
-
-    # Get player name
-    player_name = riot.account.by_puuid("EUROPE", player_puuid)["gameName"]
-    print(f"Player name: {player_name}")
-
-
-except ApiError as e:
-    print(e)
+def get_player_pseudo(puiid):
+    try:
+        player_pseudo = riot.account.by_puuid("EUROPE", puiid)
+        return player_pseudo["gameName"] + "#" + player_pseudo["tagLine"]
+    except ApiError as e:
+        print(e)
 
 if __name__ == "__main__":
+    player_puuid = "B0s7DeR8XNKCcDdFW6TpnRdY7cNBiFXpAWbhZaNk53sOkUQboogZxjwHdoCbWeOrnHxn9hH5T-aJlQ"
     assert get_player_puuid("JaimeLesGlaces", "CCLES") == player_puuid
+    assert get_player_pseudo(player_puuid) == "JaimeLesGlaces#CCLES"
