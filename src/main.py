de2d0eb2780408ed20bde4f3bc7630a06865e7d6
Rémi LAVERGNE <remi.lavergne@etu.uca.fr>
2024-05-17 11:35:07 +0200

    ➕ Add the Current Act recovery function

--- src/main.py
+++ src/main.py
@@ -1,26 +1,36 @@
 # Valorant API
-from riotwatcher import RiotWatcher, ValWatcher, LolWatcher, ApiError
+from riotwatcher import RiotWatcher, ValWatcher, ApiError
 
 YOUR_API_KEY = ""
 
 riot = RiotWatcher(YOUR_API_KEY) # Riot API
 val = ValWatcher(YOUR_API_KEY) # Valorant API
 
+# Riot User
 def get_player_puuid(name, tag):
-    try:
-        player_puuid = riot.account.by_riot_id("EUROPE", name, tag)["puuid"]
-        return player_puuid
-    except ApiError as e:
-        print("Player cannot be found.\n", e)
+    return riot.account.by_riot_id("EUROPE", name, tag)["puuid"]
 
 def get_player_pseudo(puiid):
-    try:
-        player_pseudo = riot.account.by_puuid("EUROPE", puiid)
-        return player_pseudo["gameName"] + "#" + player_pseudo["tagLine"]
-    except ApiError as e:
-        print(e)
+    player_pseudo = riot.account.by_puuid("EUROPE", puiid)
+    return player_pseudo["gameName"] + "#" + player_pseudo["tagLine"]
+
+# Valorant Ranked
+def get_current_act():
+    data = val.content.contents("EU","fr-FR")["acts"]
+    for act in data:
+        if act["isActive"]:
+            if act["type"] == "act":
+                current_act = act["name"]
+            elif act["type"] == "episode":
+                current_episode = act["name"]
+    return current_episode + " - " + current_act
+
 
 if __name__ == "__main__":
-    player_puuid = "B0s7DeR8XNKCcDdFW6TpnRdY7cNBiFXpAWbhZaNk53sOkUQboogZxjwHdoCbWeOrnHxn9hH5T-aJlQ"
-    assert get_player_puuid("JaimeLesGlaces", "CCLES") == player_puuid
-    assert get_player_pseudo(player_puuid) == "JaimeLesGlaces#CCLES"
+    try:
+        player_puuid = "B0s7DeR8XNKCcDdFW6TpnRdY7cNBiFXpAWbhZaNk53sOkUQboogZxjwHdoCbWeOrnHxn9hH5T-aJlQ"
+        assert get_player_puuid("JaimeLesGlaces", "CCLES") == player_puuid
+        assert get_player_pseudo(player_puuid) == "JaimeLesGlaces#CCLES"
+        assert get_current_act() == "ÉPISODE 8 - ACTE 3"
+    except ApiError as e:
+        print(e)
